1a9080fdf28d3a10e310e880304651142e6c21c3
vijay tej <vijay tej>
2017-02-11 11:15:55 -0800

    VJ

---
+++ src/MaxArrayElements.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class MaxArrayElements {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] tab = {12, 1, 21, 8};
+	       int max = Arrays.stream(tab).max().getAsInt();
+	       System.out.println("Max = " + max);
+	       
+	}
+
+}
---
+++ src/printnumbers.java
@@ -0,0 +1,13 @@
+
+public class printnumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int number = 1678356;
+		while (number > 0) {
+			System.out.println(number % 10);
+			number = number / 10;
+		}
+	}
+}
